5c36604f24d712765e0c140f0b8575508d9ae106
George <georgelthoward@gmail.com>
2024-04-27 21:20:19 +0000

    saving working changes before changing from a for loop to a parameter driven program.

--- serverSetup.sh
+++ serverSetup.sh
@@ -12,13 +12,20 @@ arrResults=$(curl -s ${strURL})
 
 # log file handling
 FILE_PATH='configurationLogs/'
+CURRENT_DATE=$(date +"%d-%b-%Y %H:%M")
 strTicketIds=$(echo $arrResults | jq -r '.[].ticketID')
 mkdir -p configurationLogs
+iterator=0
 for i in $strTicketIds
 do
 echo "TicketID: $i" >> $FILE_PATH/$i.log
-echo "TicketID: $i"
+echo "Start DateTime: ${CURRENT_DATE}" >> $FILE_PATH/$i.log
+echo "Requestor: $(echo $arrResults | jq -r '.['"${iterator}"'].requestor')" >> $FILE_PATH/$i.log
+echo "External IP Address: "
+echo "Hostname: "
+echo "Standard Configuration: $(echo $arrResults | jq -r '.['"${iterator}"'].standardConfig')" >> $FILE_PATH/$i.log
+iterator=$((iterator+1))
 done
 
 # debug statements
-echo $arrResults | jq '.[].ticketID'
+#echo $arrResults | jq '.[]'
